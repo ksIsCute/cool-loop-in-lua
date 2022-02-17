256f3057d8cc5fc64c8f69656b0f7c386da44d5f
ks <92867218+ksIsCute@users.noreply.github.com>
2022-02-17 13:23:11 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+i = -10000
+a = 10000
+repeat
+  i = i + 1
+  a = a - 1
+  print("String #", i)
+  print("Minus #", a)
+  print("N = ", i)
+  print("NU = ", i)
+  print("NUM = ", i)
+  print("NUMB = ", i)
+  print("NUMBE = ", i)
+  print("NUMBER = ", i)
+  print("NUMBERS = ", i)
+  print("NUMBER = ", a)
+  print("NUMBE = ", a)
+  print("NUMB = ", a)
+  print("NUM = ", a)
+  print("NU = ", a)
+  print("N = ", a)
+until(i > 9999 and a < -9999)
